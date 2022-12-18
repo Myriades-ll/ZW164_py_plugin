120e308bf9ee173e38c225a4986abf45ad694495
Myriades-ll <myriades.ll@gmail.com>
2022-12-18 16:03:23 +0100

    Add plan create

--- plugin.py
+++ plugin.py
@@ -51,6 +51,7 @@ def onStart():  # pylint:disable=invalid-name
     helpers.PluginConfig(Parameters)
 
     # now start
+    helpers.api.on_start()
     APP2.on_start(Parameters, Devices)
     # pylint:enable=undefined-variable
 
@@ -58,6 +59,7 @@ def onStart():  # pylint:disable=invalid-name
 @helpers.log_func('debug', separator_line=True)
 def onStop():  # pylint: disable=invalid-name
     """onStop"""
+    helpers.api.on_stop()
     APP2.on_stop()
 
 
