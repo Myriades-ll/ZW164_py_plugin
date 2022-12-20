03eacc2a0f66d351d4675190ed9b047f424e3360
Myriades-ll <myriades.ll@gmail.com>
2022-12-20 21:08:58 +0100

    Add Plan

--- app/app2.py
+++ app/app2.py
@@ -12,6 +12,7 @@ import Domoticz
 import helpers
 from app.devices.devices import DzDevices
 from app.mqtt.mqtt import Mqtt, MQTTResponse
+from app.plan.plan import Plan
 from app.zwave.soundswitch import CCSSEndpoint, CCSSNodes
 from app.zwave.zwave import ZwaveGateway
 from domoticz.responses import OnCommandResponse as OCDR
@@ -31,15 +32,14 @@ class App2:
     _zwave_gateway = ZwaveGateway()
     _soundswitches = CCSSNodes()
     _dz_devices = DzDevices()
-    _dom_api_plan: Optional[helpers.api.Plan] = None
+    _plan = Plan()
 
     def on_start(self: App2, parameters: dict, devices: Dict[int, Domoticz.Device]) -> None:
         """place this in `onStart`"""
         self._mqtt.on_start(parameters)
         self._dz_devices.on_start(devices)
         # Plugin plan
-        self._dom_api_plan = helpers.api.Plan(helpers.PluginConfig.plugin_name)
-        # self._dom_api_plan.create()
+        self._plan.on_start(parameters)
 
     def on_stop(self: App2) -> None:
         """place this in `onStop`"""
@@ -47,13 +47,12 @@ class App2:
 
     def on_connect(self: App2, octr: OCTR) -> None:
         """place this in `onConnect`"""
-        if octr.connection.Name == helpers.api.DOM_API_CON_NAME:
-            self._dom_api_plan.next_step()
-        else:
-            self._mqtt.on_connect(octr)
+        self._plan.on_connect(octr)
+        self._mqtt.on_connect(octr)
 
     def on_disconnect(self: App2, odtr: ODTR) -> None:
         """place this in `onConnect`"""
+        self._plan.on_disconnect(odtr)
         self._mqtt.on_disconnect(odtr)
 
     @helpers.log_func('debug', True, True)
@@ -74,17 +73,15 @@ class App2:
         """place this in `onMessage`
         this the place where the plugin starts working
         """
-        # Domoticz API response
-        if omer.connection.Name == helpers.api.DOM_API_CON_NAME:
-            self._dom_api_plan.message(omer)
-        else:
-            message: Optional[MQTTResponse] = self._mqtt.on_message(omer)
-            if isinstance(message, MQTTResponse):
-                if message.is_success():
-                    if message.Verb == "PUBLISH":
-                        self._on_publish(message)
-                else:
-                    helpers.error(message.Error)
+        # plugin plan
+        self._plan.on_message(omer)
+        message: Optional[MQTTResponse] = self._mqtt.on_message(omer)
+        if isinstance(message, MQTTResponse):
+            if message.is_success():
+                if message.Verb == "PUBLISH":
+                    self._on_publish(message)
+            else:
+                helpers.error(message.Error)
 
     # @helpers.log_func('debug', True, True)
     def _on_publish(self: App2, response: MQTTResponse) -> None:
@@ -106,9 +103,9 @@ class App2:
                     if self._soundswitches.is_complete():
                         for endpoint in self._soundswitches:
                             self._dz_devices.update(endpoint)
-                        self._dom_api_plan.add_device(
-                            self._dz_devices.get_unit_ids_list()
-                        )
+                        # self._dom_api_plan.add_device(
+                        #     self._dz_devices.get_unit_ids_list()
+                        # )
             elif not self._zwave_gateway.is_complete():
                 self._zwave_gateway.update(response.Topic, response.Payload)
                 if self._zwave_gateway.is_complete():
--- app/plan/plan.py
+++ app/plan/plan.py
@@ -4,15 +4,18 @@
 # standard libs
 from __future__ import annotations
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from enum import IntFlag, auto
-from gzip import decompress
-from json import loads
 from typing import Any, Dict, List, Optional
 
 # plugin libs
+from app.plan import http
 from Domoticz import Connection
-from helpers import debug
+from domoticz.responses import OnConnectResponse as OCTR
+from domoticz.responses import OnDisconnectResponse as ODTR
+from domoticz.responses import OnMessageResponse as OMER
+from helpers import debug, log_func
+from helpers.plugin_config import PluginConfig
 
 # pylint:disable=invalid-name
 
@@ -26,76 +29,6 @@ class PlanDatas:
     idx: str
 
 
-@dataclass
-class HTTPData:
-    """HTTPData"""
-    raw_data: bytes
-    encoded: str
-    result: list = field(default_factory=list, init=False)
-    status: str = field(default_factory=str, init=False)
-    title: str = field(default_factory=str, init=False)
-
-    def __post_init__(self: HTTPData) -> None:
-        """post init"""
-        tmp = self.raw_data
-        if self.encoded == 'gzip':
-            tmp = decompress(tmp)
-        results: Dict[str, Any] = loads(tmp)
-        for key, value in results.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-
-
-@dataclass
-class HTTPHeaders:
-    """HTTPHeaders
-    TypeError: __init__() missing 6 required positional arguments:
-    'Access_Control_Allow_Origin',
-    'Cache_Control',
-    'Connection',
-    'Content_Encoding',
-    'Keep_Alive',
-    'Pragma'
-    """
-    Access_Control_Allow_Origin: str  # '*',
-    Cache_Control: str  # 'no-cache',
-    Connection: str  # 'Keep-Alive',
-    Content_Length: str  # '401',
-    Content_Type: str  # 'application/json;charset=UTF-8',
-    Keep_Alive: str  # 'max=20, timeout=20'
-    Pragma: str  # 'no-cache',
-    Content_Encoding: str = field(default_factory=str)  # 'gzip',
-
-
-@dataclass
-class HTTPResponse:
-    """HTTPResponse"""
-    Data: bytes
-    Headers: Dict[str, Any]
-    Status: str
-    datas: HTTPData = field(default=None, init=False)
-    headers: HTTPHeaders = field(default=None, init=False)
-
-    def __post_init__(self: HTTPResponse) -> None:
-        """post init"""
-        if self.Status == '200':
-            tmp = {}
-            for old_key, value in self.Headers.items():
-                new_key = old_key.replace('-', '_')
-                tmp.update({new_key: value})
-            self.headers = HTTPHeaders(**tmp)
-            self.datas = HTTPData(
-                raw_data=self.Data,
-                encoded=self.headers.Content_Encoding
-            )
-        else:
-            debug(f'<HTTPResponse.__post_init__> Status: {self.Status}')
-            debug(f'<HTTPResponse.__post_init__> Headers: {self.Headers}')
-            debug(f'<HTTPResponse.__post_init__> Data: {self.Data}')
-
-# pylint:enable=invalid-name
-
-
 class PlanSteps(IntFlag):
     """Plans steps"""
     GET_PLANS = auto()
@@ -107,6 +40,14 @@ class PlanSteps(IntFlag):
 
 class Plan:
     """Classe Domoticz JSON/API:Plan"""
+    HEADERS = {
+        'Accept': 'application/json',
+        'Accept-Charset': 'utf-8',
+        'Accept-Encoding': 'gzip',
+        'Connection': 'keep-alive',
+        'Host': '127.0.0.1',
+        'User-Agent': f'Domoticz/{PluginConfig.domoticz_version}'
+    }
 
     def __init__(self: Plan) -> None:
         """initialisation de la classe"""
@@ -114,25 +55,61 @@ class Plan:
         self._finished = False
         self._con: Optional[Connection] = None
         self._plan_id = 0
+        self.plan_name = ""
 
-    def on_start(self: Plan) -> None:
+    def on_start(self: Plan, parameters: Dict[str, Any]) -> None:
         """on_start"""
-        self._con = Connection("DOM_API", 'HTTP', 'Protocol')
+        self.plan_name = parameters.get('Mode1')
+        if self.plan_name:
+            self._con = Connection(
+                self.plan_name,
+                'TCP/IP',
+                'HTTP',
+                Address='127.0.0.1',
+                Port='8080'
+            )
+            self._con.Connect()
 
-    def on_connect(self: Plan) -> None:
+    def on_connect(self: Plan, octr: OCTR) -> None:
         """on_connect"""
+        if self._plan_id == 0:
+            # TODO: (1) - retrive plan_id
+            # Headers = {"Connection": "keep-alive", "Accept": "Content-Type: text/html; charset=UTF-8"}
+            # myConn.Send({"Verb":"GET", "URL":"/page.html", "Headers": Headers})
+            self._con.Send(
+                Verb='GET',
+                URL='/json.htm?type=plans',
+                Headers=self.HEADERS
+            )
 
-    def on_disconnect(self: Plan) -> None:
+    def on_disconnect(self: Plan, odtr: ODTR) -> None:
         """on_disconnect"""
-        if isinstance(self._con, Connection):
+        if self._check_con(odtr.connection):
             if not self._finished:
                 self._con.Connect()
 
-    def on_message(self: Plan) -> None:
-        """on_message"""
+    @log_func('debug', True, True)
+    def on_message(self: Plan, omer: OMER) -> None:
+        """on_message
+        #ignore_self_arg
+        """
+        if self._check_con(omer.connection):
+            pass
 
-    def set_device_list(self: Plan, device_list: List[int]) -> None:
+    def add_device(self: Plan, device_list: List[int]) -> None:
         """set_device_list
         @arg device_list (List[int]): liste des device idx de Domoticz
         """
         self._devices.update(device_list)
+
+    def del_device(self: Plan, device_id: int) -> None:
+        """del_device"""
+        # TODO: (latest) - remove device from plan
+        self._devices.discard(device_id)
+
+    def _check_con(self: Plan, connection: Connection) -> bool:
+        """_check_con"""
+        if isinstance(self._con, Connection):
+            if connection.Name == self.plan_name:
+                return True
+        return False
