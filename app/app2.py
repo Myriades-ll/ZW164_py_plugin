83ac8d37b1f50dd009326af9d4f7baccd220f5f2
Myriades-ll <myriades.ll@gmail.com>
2022-12-23 07:46:22 +0100

    change debug

--- app/app2.py
+++ app/app2.py
@@ -103,7 +103,7 @@ class App2:
                     if self._soundswitches.is_complete():
                         for endpoint in self._soundswitches:
                             self._dz_devices.update(endpoint)
-                        helpers.status(self._dz_devices._devices)
+                        helpers.status(self._dz_devices._devices[1])
                         # self._plan.add_device(
                         #     self._dz_devices.get_device_idxs_list()
                         # )
