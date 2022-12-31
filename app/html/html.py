2a4a16d23a337e999f9ebd6e4ed76f72deaee682
Myriades-ll <myriades.ll@gmail.com>
2022-12-31 08:50:56 +0100

    Add Html

--- app/html/html.py
+++ app/html/html.py
@@ -3,8 +3,13 @@
 
 # standard libs
 from __future__ import annotations
-from shutil import copy2
+
 import os
+from shutil import copy2
+
+# plugin libs
+from domoticz.parameters import PluginParameters
+import helpers
 
 HTML_NAME = 'ZW164_py_plugin'
 HTML_SOUCRE = f'./plugins/{HTML_NAME}/app/html/index.html'
@@ -13,12 +18,23 @@ HTML_DEST = f'./www/templates/{HTML_NAME}.html'
 
 class HtmlPage:
     """The Html page"""
+    parameters: PluginParameters
 
-    def on_start(self: HtmlPage) -> None:
+    def on_start(self: HtmlPage, parameters: PluginParameters) -> None:
         """onStart event"""
-        copy2(HTML_SOUCRE, HTML_DEST)
+        self.parameters = parameters
+        self.__install()
 
     def on_stop(self: HtmlPage) -> None:
         """onStop event"""
-        if os.path.exists(HTML_DEST):
-            os.remove(HTML_DEST)
+        self.__uninstall()
+
+    def __install(self: HtmlPage) -> None:
+        """installation des fichiers"""
+        # copy2(HTML_SOUCRE, HTML_DEST)
+        src_path = os.path.join(self.parameters.HomeFolder, 'app/html/files')
+        helpers.debug(src_path)
+        # for root, dirs, files in os.walk(top)
+
+    def __uninstall(self: HtmlPage) -> None:
+        """suppression des fichiers"""
