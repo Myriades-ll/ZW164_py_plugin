297628fd05b708dd95c84399b07044f86b86a74a
Myriades-ll <myriades.ll@gmail.com>
2023-01-01 11:02:47 +0100

    Add HTML

--- app/html/files/ccss.js
+++ app/html/files/ccss.js
@@ -1,6 +1,9 @@
 var canvas = document.getElementById('draw');
 console.log(canvas);
 var ctx = canvas.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
+ctx.moveTo(0, 100);
+ctx.lineTo(100, 0);
+ctx.lineTo(200, 0);
+ctx.lineTo(300, 100);
+ctx.lineTo(400, 100);
 ctx.stroke();
