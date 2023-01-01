283a2f6f185618392a8bda5636b87c679cb5c338
Myriades-ll <myriades.ll@gmail.com>
2023-01-01 11:34:36 +0100

    Create courbe class

--- app/html/files/ccss.js
+++ app/html/files/ccss.js
@@ -1,9 +1,19 @@
-var canvas = document.getElementById('draw');
-console.log(canvas);
-var ctx = canvas.getContext("2d");
-ctx.moveTo(0, 100);
-ctx.lineTo(100, 0);
-ctx.lineTo(200, 0);
-ctx.lineTo(300, 100);
-ctx.lineTo(400, 100);
-ctx.stroke();
+class Courbe {
+
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.context = this.canvas.getContext('2d');
+    }
+
+    draw() {
+        ctx.moveTo(0, 100);
+        ctx.lineTo(100, 0);
+        ctx.lineTo(200, 0);
+        ctx.lineTo(300, 100);
+        ctx.lineTo(400, 100);
+        ctx.stroke();
+    }
+}
+
+let courbe = new Courbe(document.getElementById('draw'));
+courbe.draw();
