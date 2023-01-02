07d17d6808ea128a8d8ec95ff085d85377457eeb
Myriades-ll <myriades.ll@gmail.com>
2023-01-02 09:26:16 +0100

    Add types.js

--- app/html/files/ccss.js
+++ app/html/files/ccss.js
@@ -1,32 +1,4 @@
-class ArrayOfNumbers extends Array {
-    constructor() {
-        super();
-    }
-
-    pushNumber(item) {
-        item *= 1;
-        if (isFinite(item)) this.push(item);
-    }
-
-    sum() {
-        return this.reduce(
-            function (accumulator, currentValue) {
-                return accumulator + currentValue;
-            }, 0
-        )
-    }
-
-    non_zeros() {
-        return this.reduce(
-            function (accumulator, currentValue) {
-                if (currentValue > 0) {
-                    return accumulator + 1;
-                }
-                return accumulator;
-            }, 0
-        )
-    }
-}
+import { ArrayOfNumbers } from 'templates/ccss_libs/types.js';
 
 class Courbe {
 
@@ -42,7 +14,7 @@ class Courbe {
     }
 
     #init() {
-        this.redraw([5, 1, 5, 1]);
+        this.redraw([1, 1, 1, 1]);
     }
 
     #normalize_value(value, min, max) {
@@ -56,32 +28,33 @@ class Courbe {
         let y_value = this.#canvas_height;
         this.#context.moveTo(0, y_value);
         let deltas = new ArrayOfNumbers();
-        timed_values.forEach(
-            (item, index) => {
-                let delta_time = 0.0;
-                // normalize time periods
-                if (index % 2 == 0) {// 20ms; from 0 to 127
-                    delta_time += this.#normalize_value(item, 0, 127) * 0.02;
-                } else {// 100ms; from 0 to 255
-                    delta_time += this.#normalize_value(item, 0, 255) * 0.1;
-                }
-                deltas.pushNumber(delta_time)
-            }
-        );
+        deltas.pushNumber(timed_values);
+        // timed_values.forEach(
+        //     (item, index) => {
+        //         let delta_time = 0.0;
+        //         // normalize time periods
+        //         if (index % 2 == 0) {// 20ms; from 0 to 127
+        //             delta_time += this.#normalize_value(item, 0, 127) * 0.02;
+        //         } else {// 100ms; from 0 to 255
+        //             delta_time += this.#normalize_value(item, 0, 255) * 0.1;
+        //         }
+        //         deltas.pushNumber(delta_time)
+        //     }
+        // );
         let total_time = deltas.sum();
         let non_zeros = deltas.non_zeros();
-        deltas.forEach(
+        console.log('total time', total_time);
+        console.log('Non zeros values', non_zeros);
+        timed_values.forEach(
             (item, index) => {
                 if (y_value == 0) y_value = this.#canvas_height;
                 else y_value = 0;
                 this.#context.moveTo(
-                    (index + 1) * this.#canvas_width / non_zeros,
+                    (index + 1) * this.#canvas_width * item / total_time,
                     y_value
                 );
             }
         )
-        console.log('total time', total_time);
-        console.log('Non zeros values', non_zeros);
         // last point; fixed
         this.#context.moveTo(this.#canvas_width, this.#canvas_height);
         this.#context.stroke();
