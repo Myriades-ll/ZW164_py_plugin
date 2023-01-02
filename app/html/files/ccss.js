cf5780e1f33260600425756cbd976d03ac0e08c3
Myriades-ll <myriades.ll@gmail.com>
2023-01-02 10:52:52 +0100

    Write redraw

--- app/html/files/ccss.js
+++ app/html/files/ccss.js
@@ -3,11 +3,37 @@ class ArrayOfNumbers extends Array {
         super();
     }
 
-    pushNumber(item) {
-        item *= 1;
-        if (isFinite(item)) this.push(item);
+    /**
+     * 
+     * @param {Array<any>} item 
+     * @returns {boolean}
+     */
+    isFiniteArray(item) {
+        if (item instanceof Array) {
+            return item.reduce(
+                (accumulator, currentValue) => { return accumulator &&= isFinite(currentValue) },
+                true
+            )
+        }
+        return false
     }
 
+    /**
+     * @param {number | Array<number>} item
+     */
+    pushNumber(item) {
+        if (item instanceof Array) {
+            if (this.isFiniteArray(item)) this.concat(item);
+            else throw new Error("Not number values " + item)
+        } else {
+            item *= 1;
+            if (isFinite(item)) this.push(item);
+        }
+    }
+
+    /**
+     * @returns {number} the sum of all values
+     */
     sum() {
         return this.reduce(
             (accumulator, currentValue) => {
@@ -17,22 +43,13 @@ class ArrayOfNumbers extends Array {
         ), 0
     }
 
-    sum(valueNumber) {
-        this.forEach(
-            (value, index) => {
-                console.log('sum(valueNumber)', value, index);
-                this[index] = value + valueNumber;
-            }
-        )
-    }
-
+    /**
+     * @returns {number} the number of values that are non zeros
+     */
     non_zeros() {
         return this.reduce(
             (accumulator, currentValue) => {
-                if (currentValue > 0) {
-                    return accumulator + 1;
-                }
-                return accumulator;
+                return currentValue > 0 ? accumulator + 1 : accumulator;
             }, 0
         )
     }
@@ -43,63 +60,70 @@ class ArrayOfNumbers extends Array {
 }
 
 class Courbe {
+    #canvas_height;
+    #canvas_width;
+    #context;
 
-    #canvas_height = 0;
-    #canvas_width = 0;
-    #context = undefined;
+    /**
+     * @param {HTMLCanvasElement} canvas
+     */
     constructor(canvas) {
         this.canvas = canvas;
-        this.#context = this.canvas.getContext('2d');
-        this.#canvas_height = this.canvas.height;
-        this.#canvas_width = this.canvas.width;
-        this.#init();
+        if (this.#is_canvas()) {
+            this.#context = this.canvas.getContext('2d');
+            this.#canvas_height = this.canvas.height;
+            this.#canvas_width = this.canvas.width;
+            this.#init();
+        }
+    }
+
+    #is_canvas() {
+        return this.canvas instanceof HTMLCanvasElement;
     }
 
     #init() {
         this.redraw([1, 1, 1, 1]);
     }
 
+    /**
+     * @param {number} value
+     * @param {number} min
+     * @param {number} max
+     */
     #normalize_value(value, min, max) {
         return Math.max(Math.min(value, max), min);
     }
 
+    /**
+     * 
+     * @param {Array<number>} timed_values 
+     */
     redraw(timed_values) {
-        this.#context.clearRect(0, 0, this.canvas_width, this.canvas_height);
-        this.#context.beginPath();
-        // first point; fixed
-        let y_value = this.#canvas_height;
-        this.#context.moveTo(0, y_value);
-        let deltas = new ArrayOfNumbers();
-        deltas.pushNumber(timed_values);
-        // timed_values.forEach(
-        //     (item, index) => {
-        //         let delta_time = 0.0;
-        //         // normalize time periods
-        //         if (index % 2 == 0) {// 20ms; from 0 to 127
-        //             delta_time += this.#normalize_value(item, 0, 127) * 0.02;
-        //         } else {// 100ms; from 0 to 255
-        //             delta_time += this.#normalize_value(item, 0, 255) * 0.1;
-        //         }
-        //         deltas.pushNumber(delta_time)
-        //     }
-        // );
-        let total_time = deltas.sum();
-        let non_zeros = deltas.non_zeros();
-        console.log('total time', total_time);
-        console.log('Non zeros values', non_zeros);
-        timed_values.forEach(
-            (item, index) => {
-                if (y_value == 0) y_value = this.#canvas_height;
-                else y_value = 0;
-                this.#context.moveTo(
-                    (index + 1) * this.#canvas_width * item / total_time,
-                    y_value
-                );
-            }
-        )
-        // last point; fixed
-        this.#context.moveTo(this.#canvas_width, this.#canvas_height);
-        this.#context.stroke();
+        if (this.#is_canvas()) {
+            this.#context.clearRect(0, 0, this.#canvas_width, this.#canvas_height);
+            this.#context.beginPath();
+            // first point; fixed
+            let y_value = this.#canvas_height;
+            this.#context.moveTo(0, y_value);
+            let deltas = new ArrayOfNumbers();
+            deltas.pushNumber(timed_values);
+            let total_time = deltas.sum();
+            let non_zeros = deltas.non_zeros();
+            console.log('total time', total_time);
+            console.log('Non zeros values', non_zeros);
+            deltas.forEach(
+                (item, index) => {
+                    y_value == 0 ? y_value = this.#canvas_height : y_value = 0;
+                    this.#context.moveTo(
+                        (index + 1) * this.#canvas_width * item / total_time,
+                        y_value
+                    );
+                }
+            )
+            // last point; fixed
+            this.#context.moveTo(this.#canvas_width, this.#canvas_height);
+            this.#context.stroke();
+        }
     }
 }
 
